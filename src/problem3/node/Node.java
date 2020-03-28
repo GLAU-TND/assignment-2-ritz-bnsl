3c948f61d8a61dd7e34ffd29051ea32a5377ef22
Ritika Bansal <ritika.bansal_cs18@gla.ac.in>
2020-03-28 15:38:23 +0530

    Update Node.java

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -5,6 +5,31 @@
  *  Time: 7:13 PM
  */
 package problem3.node;
+import problem5.student.Student;
 
 public class Node {
+   private Student student;
+    private Node nextNode;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    @Override
+    public String toString() {
+        return "Node " + "\n" +
+                getStudent();
+    }
 }
